df84d94f0c4a44b4c85cb3dc178cf48a8d1ba6b6
Velmurugan Gopalakrishnan <velmurugangopal06@gmail.com>
2026-07-31 23:38:06 +0530

    (feat) Click event handling done

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,6 +1,13 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["Chennai", "Tirunelveli", "Madurai", "Coimbatore", "Salem"];
   //   items = [];
+
+  let handleItemClick = (item: string, event: MouseEvent) => {
+    console.log("Item Clicked", item);
+    console.log("Event", event);
+  };
 
   return (
     <>
@@ -8,7 +15,11 @@ function ListGroup() {
       {items.length === 0 && <p>No items to display</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className="list-group-item" key={index}>
+          <li
+            className="list-group-item"
+            key={index}
+            onClick={(event) => handleItemClick(item, event)}
+          >
             {item}
           </li>
         ))}
