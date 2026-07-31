ba8affd68e5230eadedb71ea8eacfea57a90fdf5
Velmurugan Gopalakrishnan <velmurugangopal06@gmail.com>
2026-07-31 23:28:10 +0530

    (feat) conditional rendering done

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,9 +1,11 @@
 function ListGroup() {
-  const items = ["Chennai", "Tirunelveli", "Madurai", "Coimbatore", "Salem"];
+  let items = ["Chennai", "Tirunelveli", "Madurai", "Coimbatore", "Salem"];
+  //   items = [];
 
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No items to display</p>}
       <ul className="list-group">
         {items.map((item, index) => (
           <li className="list-group-item" key={index}>
